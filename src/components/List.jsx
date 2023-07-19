a1c39d5f24222d59f75f4eab6fe7ffa2ee801649
Celal Mehmet Çetin <72734602+celalcetin27@users.noreply.github.com>
2023-07-19 17:02:55 +0300

    nn

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -27,7 +27,7 @@ const List = () => {
             setResults(domainProps)
 
         } catch (error) {
-            console.log("error : " ,error);
+            console.log(error);
         }
     }
 
